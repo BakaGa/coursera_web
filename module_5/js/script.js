455aed68aa204e53860592b974d95576181482c3
Владимир Диев <mrphantom@MacBook-Pro-Vladimir-2.local>
2020-08-16 11:37:00 +0700

    module_5_minor

--- module_5/js/script.js
+++ module_5/js/script.js
@@ -92,7 +92,7 @@ $(function() {
             // $dc.loadMenuItems('L')
             // Hint: you need to surround the chosen category short name with something before inserting
             // it into the home html snippet.
-            chosenCategoryShortName = chosenCategoryShortName.replace(new RegExp(chosenCategoryShortName, "g"), "'" + chosenCategoryShortName + "'");
+            chosenCategoryShortName =  "'" + chosenCategoryShortName + "'";
             var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
             // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
             // Use the existing insertHtml function for that purpose. Look through this code for an example
